e1ea8912b665efa1940377454b8336aadb193097
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2016-01-10 18:11:51 +0000

    Added config loading. respects XDG, commented out the old input fields. Should we keep them?

--- spoon.go
+++ spoon.go
@@ -1,44 +1,52 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
-	. "github.com/rthornton128/goncurses"
 	"os"
-	"strings"
+        "io"
+        "log"
+	//"strings"
 	"time"
+        "encoding/json"
+        "launchpad.net/go-xdg"
 	"github.com/ChimeraCoder/anaconda"
+	. "github.com/rthornton128/goncurses"
 )
 
 func main() {
-
 	// check to see if the config file exists
 	// if it doesn't, create it and ask for API keys
 
-	if _, err := os.Stat("$HOME/.config/spoon/config"); err != nil {
+	var tweets []anaconda.Tweet
+        conf := loadConfig()
+        api, tweets = createAPI(conf.Twitter.Api_key, conf.Twitter.Api_secret, 
+                                conf.Twitter.Access_token, conf.Twitter.Access_secret)
+
+	if _, err := xdg.Config.Find("/spoon/config.json"); err != nil {
 		fmt.Println(`Hello there! It looks like this is your first time running spoon
 			or you've misplaced your configuration file. We're gonna need those Twitter keys
 			again - actually, do you even use Twitter?`)
 	}
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("y/n: ")
-	text, _ := reader.ReadString('\n')
-	var tweets []anaconda.Tweet
-	if strings.TrimSpace(text) == "y" {
-		fmt.Println(`Alright, now hand over your keys.
-			Consumer key first, then the secret consumer key.`)
-		ck, _ := reader.ReadString('\n')
-		csk, _ := reader.ReadString('\n')
-		fmt.Println(`Okay, great. Now we'll also need your Access Token and
-			Access Token Secret to fire up the reader.`)
-		at, _ := reader.ReadString('\n')
-		ats, _ := reader.ReadString('\n')
-		ck = strings.TrimSpace(ck)
-		csk = strings.TrimSpace(csk)
-		at = strings.TrimSpace(at)
-		ats = strings.TrimSpace(ats)
-		api, tweets = createAPI(ck, csk, at, ats)
-	}
+
+	//reader := bufio.NewReader(os.Stdin)
+	//fmt.Print("y/n: ")
+	//text, _ := reader.ReadString('\n')
+	//if strings.TrimSpace(text) == "y" {
+	//	fmt.Println(`Alright, now hand over your keys.
+	//		Consumer key first, then the secret consumer key.`)
+	//	ck, _ := reader.ReadString('\n')
+	//	csk, _ := reader.ReadString('\n')
+	//	fmt.Println(`Okay, great. Now we'll also need your Access Token and
+	//		Access Token Secret to fire up the reader.`)
+	//	at, _ := reader.ReadString('\n')
+	//	ats, _ := reader.ReadString('\n')
+	//	ck = strings.TrimSpace(ck)
+	//	csk = strings.TrimSpace(csk)
+	//	at = strings.TrimSpace(at)
+	//	ats = strings.TrimSpace(ats)
+	//	api, tweets = createAPI(ck, csk, at, ats)
+	//}
 
 	stdscr, err := Init()
 	if err != nil {
@@ -111,4 +119,23 @@ main:
 
 		}
 	}
+}
+
+func loadConfig() Config{
+    conf := Config{}
+    var file io.Reader
+    path, err := xdg.Config.Ensure("spoon/config.json")
+    if err == nil {
+        file, _ = os.Open(path)
+        fmt.Println(path)
+    } else {
+        fmt.Println(err.Error)
+    }
+    decoder := json.NewDecoder(file)
+    err = decoder.Decode(&conf)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(conf)
+    return conf
 }
